21e3ad745b7f732d398df1c9ea4f171c8bd1c9b7
Anh Phan <anhphanvt2705@gmail.com>
2024-07-09 15:04:18 +0700

    upload day 1

---
+++ day_1/example.py
@@ -0,0 +1,9 @@
+name = ""
+age = 0
+gender = ""
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+gender = input("Enter your gender: ")
+
+print(f"You are a {gender}. Your age is {age}. Congratulations {name}! You have successfully run your first python program.")
